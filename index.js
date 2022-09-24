d86238b551c054c986a5850db4f4e1cc4be776a9
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2022-09-24 17:08:43 +0200

    added 'express.patch'

--- index.js
+++ index.js
@@ -55,6 +55,15 @@ app.get('/comments/:id', (req, res) => {
   res.render('comments/show', { comment });
 });
 
+app.patch('/comments/:id', (req, res) => {
+  const { id } = req.params;
+  const newCommentText = req.body.comment;
+  const foundComment = comments.find((c) => c.id === id);
+  foundComment.comment = newCommentText;
+  //   res.send('it worked');
+  res.redirect('/comments');
+});
+
 app.get('/tacos', (req, res) => {
   res.send('GET /tacos response');
 });
