a4efe541c8eb30321746aa8fc12388bd3e1b4a94
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2022-09-24 15:52:08 +0200

    Added UUID for the content

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const { v4: uuid } = require('uuid');
+uuid();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,22 +13,22 @@ app.set('views', path.join(__dirname + '/views'));
 
 const comments = [
   {
-    id: 1,
+    id: uuid(),
     username: 'Todd',
     comment: 'lol that is so funny!',
   },
   {
-    id: 2,
+    id: uuid(),
     username: 'Skyler',
     comment: 'I like to go birdwatching with my dog',
   },
   {
-    id: 3,
+    id: uuid(),
     username: 'Sk8erBoi',
     comment: 'Plz delete your account, Todd',
   },
   {
-    id: 4,
+    id: uuid(),
     username: 'onlysayswoof',
     comment: 'woof woof woof',
   },
@@ -43,13 +45,13 @@ app.get('/comments/new', (req, res) => {
 app.post('/comments', (req, res) => {
   console.log(req.body);
   const { username, comment } = req.body;
-  comments.push({ username, comment });
+  comments.push({ username, comment, id: uuid() });
   res.redirect('/comments');
 });
 
 app.get('/comments/:id', (req, res) => {
   const { id } = req.params;
-  const comment = comments.find((c) => c.id === parseInt(id));
+  const comment = comments.find((c) => c.id === id);
   res.render('comments/show', { comment });
 });
 
