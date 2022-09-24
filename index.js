c3901bbfee495df19bf68d8ee9833ca16e4c27f1
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2022-09-24 14:54:25 +0200

    first commit/alredy some practicing added

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname + '/views'));
+
+const comments = [
+  {
+    username: 'Todd',
+    comment: 'lol that is so funny!',
+  },
+  {
+    username: 'Skyler',
+    comment: 'I like to go birdwatching with my dog',
+  },
+  {
+    username: 'Sk8erBoi',
+    comment: 'Plz delete your account, Todd',
+  },
+  {
+    username: 'onlysayswoof',
+    comment: 'woof woof woof',
+  },
+];
+
+app.get('/comments', (req, res) => {
+  res.render('comments/index', { comments });
+});
+
+app.get('/comments/new', (req, res) => {
+  res.render('comments/new');
+});
+
+app.post('/comments', (req, res) => {
+  console.log(req.body);
+  const { username, comment } = req.body;
+  comments.push({ username, comment });
+  res.redirect('/comments');
+});
+
+app.get('/tacos', (req, res) => {
+  res.send('GET /tacos response');
+});
+
+app.post('/tacos', (req, res) => {
+  const { meat, qty } = req.body;
+  res.send(`Okay here are your ${qty} ${meat} tacos.`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
