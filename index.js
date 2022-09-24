5e862608c078f03cfcc915b381a289cb6612a37c
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2022-09-24 18:18:16 +0200

    added method-override so it is possible to patch, not using postman

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const methodOverride = require('method-override');
+const { v4: uuid } = require('uuid');
+
 const app = express();
 const port = 3000;
 const path = require('path');
-const { v4: uuid } = require('uuid');
 uuid();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname + '/views'));
@@ -53,6 +57,12 @@ app.get('/comments/:id', (req, res) => {
   const { id } = req.params;
   const comment = comments.find((c) => c.id === id);
   res.render('comments/show', { comment });
+});
+
+app.get('/comments/:id/edit', (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === req.params.id);
+  res.render('comments/edit', { comment });
 });
 
 app.patch('/comments/:id', (req, res) => {
